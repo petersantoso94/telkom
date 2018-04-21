943c77ab3b1e2c1d251fce6696bd403154e3f854
petersantoso94 <petersantoso94@gmail.com>
2018-04-21 16:20:13 +0800

    reset reporting 4-21-2018

--- app/views/resetreporting.blade.php
+++ app/views/resetreporting.blade.php
@@ -17,16 +17,16 @@
                     <?php if (isset($success)) { ?>
                         <div class="alert alert-success alert-dismissible" role="alert" style="width: 98%; margin: 1%">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            Successfully resetting all reporting data.
+                            Successfully resetting Churn data.
                         </div>
                     <?php } ?>
                 </div>
                 <div class="row">
                     <div class="col-xs-12">
-                        <h4>Reset ALL Reporting</h4>
+                        <h4>Reset Churn Reporting</h4>
                     </div>
                 </div>
-                <input type="hidden" name='jenis' value='ivr'>
+                <input type="hidden" name='jenis' value='reset_churn'>
                 <div class="row margtop20">
                     <div class="col-xs-8">
                         <button type="button" id='btn-submit-ivr' class="button btnblue btn-wide wide-h" style="background-color: #424242; color: white;">Submit</button>
@@ -35,8 +35,109 @@
             </div>
         </form>
     </div>
+    <div class='col-xs-8'>
+        <form method="POST" accept-charset="UTF-8" enctype="multipart/form-data" id='form-reset-productive'>
+            <div class="white-pane__bordered margbot20">
+                <div class="row">
+                    <?php if (isset($successp)) { ?>
+                        <div class="alert alert-success alert-dismissible" role="alert" style="width: 98%; margin: 1%">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            Successfully resetting productive data.
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h4>Reset Productive Reporting</h4>
+                    </div>
+                </div>
+                <input type="hidden" name='jenis' value='reset_prod'>
+                <div class="row margtop20">
+                    <div class="col-xs-8">
+                        <button type="button" id='btn-submit-reset-prod' class="button btnblue btn-wide wide-h" style="background-color: #424242; color: white;">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class='col-xs-8'>
+        <form method="POST" accept-charset="UTF-8" enctype="multipart/form-data" id='form-reset-ivr'>
+            <div class="white-pane__bordered margbot20">
+                <div class="row">
+                    <?php if (isset($successi)) { ?>
+                        <div class="alert alert-success alert-dismissible" role="alert" style="width: 98%; margin: 1%">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            Successfully resetting IVR data.
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h4>Reset IVR Reporting</h4>
+                    </div>
+                </div>
+                <input type="hidden" name='jenis' value='reset_ivr'>
+                <div class="row margtop20">
+                    <div class="col-xs-8">
+                        <button type="button" id='btn-submit-reset-ivr' class="button btnblue btn-wide wide-h" style="background-color: #424242; color: white;">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
-
+<div class="row">
+    <div class='col-xs-8'>
+        <form method="POST" accept-charset="UTF-8" enctype="multipart/form-data" id='form-reset-act'>
+            <div class="white-pane__bordered margbot20">
+                <div class="row">
+                    <?php if (isset($successa)) { ?>
+                        <div class="alert alert-success alert-dismissible" role="alert" style="width: 98%; margin: 1%">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            Successfully resetting Aqcuisition Date.
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h4>Reset Aqcuisition Date Reporting</h4>
+                    </div>
+                </div>
+                <input type="hidden" name='jenis' value='reset_act'>
+                <div class="row margtop20">
+                    <div class="col-xs-8">
+                        <button type="button" id='btn-submit-reset-act' class="button btnblue btn-wide wide-h" style="background-color: #424242; color: white;">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class='col-xs-8'>
+        <form method="POST" accept-charset="UTF-8" enctype="multipart/form-data" id='form-reset-top'>
+            <div class="white-pane__bordered margbot20">
+                <div class="row">
+                    <?php if (isset($successt)) { ?>
+                        <div class="alert alert-success alert-dismissible" role="alert" style="width: 98%; margin: 1%">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            Successfully resetting Top Up data.
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h4>Reset Top Up Reporting</h4>
+                    </div>
+                </div>
+                <input type="hidden" name='jenis' value='reset_top'>
+                <div class="row margtop20">
+                    <div class="col-xs-8">
+                        <button type="button" id='btn-submit-reset-top' class="button btnblue btn-wide wide-h" style="background-color: #424242; color: white;">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 @stop
 @section('js-content')
 <script type="text/javascript" src="{{Asset('lib/bootstrap/js/jquery.dataTables.min.js')}}"></script>
@@ -45,6 +146,18 @@
 <script>
 $('#btn-submit-ivr').on('click', function (e) {
     document.getElementById("form-ivr-purchase").submit();
+});
+$('#btn-submit-reset-prod').on('click', function (e) {
+    document.getElementById("form-reset-productive").submit();
+});
+$('#btn-submit-reset-ivr').on('click', function (e) {
+    document.getElementById("form-reset-ivr").submit();
+});
+$('#btn-submit-reset-act').on('click', function (e) {
+    document.getElementById("form-reset-act").submit();
+});
+$('#btn-submit-reset-top').on('click', function (e) {
+    document.getElementById("form-reset-top").submit();
 });
 </script>
 @stop
