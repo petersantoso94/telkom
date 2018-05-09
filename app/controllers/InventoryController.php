899895365c42a1ae726419b36d4d1f6d15ae030b
petersantoso94 <petersantoso94@gmail.com>
2018-05-09 23:31:01 +0800

    export excel 9-5-2018

--- app/controllers/InventoryController.php
+++ app/controllers/InventoryController.php
@@ -1645,7 +1645,7 @@ class InventoryController extends BaseController {
                     }
                 }
                 foreach ($data as $key->$a) {
-                    $myArr = array($stats,);
+                    $myArr = array($key,$a[0],$a[1],$a[2],$a[3],$a[4],$a[5],$a[6],$a[7],$a[8],$a[9],$a[10],$a[11]);
                     $writer->addRow($myArr); // add a row at a time
                 }
             }
