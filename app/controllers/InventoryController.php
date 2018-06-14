c44e080970f2825ea7030ccb755d8686d9ee8e57
petersantoso94 <petersantoso94@gmail.com>
2018-06-14 11:53:54 +0800

    reset reporting 14-6-2018

--- app/controllers/InventoryController.php
+++ app/controllers/InventoryController.php
@@ -950,7 +950,7 @@ class InventoryController extends BaseController {
                 DB::delete('DELETE FROM `r_stats` WHERE Status LIKE "%topup%"');
                 DB::update('UPDATE `m_inventory` SET `TopUpDate`=NULL, TopUpMSISDN=NULL WHERE 1');
                 return View::make('resetreporting')->withPage('reset reporting')->withSuccesst('ok');
-            }else if (Input::get('jenis') == 'reset_top') {
+            }else if (Input::get('jenis') == 'reset_sip') {
                 DB::update('UPDATE `m_inventory` SET SET `ActivationName`= NULL,`ActivationStore`= NULL WHERE 1');
                 return View::make('resetreporting')->withPage('reset reporting')->withSuccesssip('ok');
             }
