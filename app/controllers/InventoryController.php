bdf55fd9be921830363a07b2060c8b2468367492
petersantoso94 <petersantoso94@gmail.com>
2018-06-27 14:15:58 +0800

    admin panel 26-6-2018

--- app/controllers/InventoryController.php
+++ app/controllers/InventoryController.php
@@ -2265,7 +2265,7 @@ class InventoryController extends BaseController {
                                 number_format($a[7], 2, '.', ''), number_format($a[8], 2, '.', ''),
                                 number_format($a[9], 2, '.', ''), number_format($a[10], 2, '.', ''), number_format($a[11], 2, '.', ''));
                         else
-                            $myArr = array($key, number_format($a[0]), number_format($a[1]), number_format($a[2]), number_format($a[3]), number_format($a[4]), number_format($a[5]), number_format($a[6]), number_format($a[7]), number_format($a[8]), number_format($a[9]), number_format($a[10]), number_format($a[11]));
+                            $myArr = array($key2, number_format($a[0]), number_format($a[1]), number_format($a[2]), number_format($a[3]), number_format($a[4]), number_format($a[5]), number_format($a[6]), number_format($a[7]), number_format($a[8]), number_format($a[9]), number_format($a[10]), number_format($a[11]));
                         $writer->addRow($myArr); // add a row at a time
                     }
                 }
@@ -2362,7 +2362,7 @@ class InventoryController extends BaseController {
                                 number_format($a[7], 2, '.', ''), number_format($a[8], 2, '.', ''),
                                 number_format($a[9], 2, '.', ''), number_format($a[10], 2, '.', ''), number_format($a[11], 2, '.', ''));
                         else
-                            $myArr = array($key, number_format($a[0]), number_format($a[1]), number_format($a[2]), number_format($a[3]), number_format($a[4]), number_format($a[5]), number_format($a[6]), number_format($a[7]), number_format($a[8]), number_format($a[9]), number_format($a[10]), number_format($a[11]));
+                            $myArr = array($key2, number_format($a[0]), number_format($a[1]), number_format($a[2]), number_format($a[3]), number_format($a[4]), number_format($a[5]), number_format($a[6]), number_format($a[7]), number_format($a[8]), number_format($a[9]), number_format($a[10]), number_format($a[11]));
                         $writer->addRow($myArr); // add a row at a time
                     }
                 }
