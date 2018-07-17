6b58fc8cb5658fbcd8bc6b94c4309c3417cdf685
petersantoso94 <petersantoso94@gmail.com>
2018-07-17 12:55:34 +0800

    17-7-2018

--- app/controllers/InventoryController.php
+++ app/controllers/InventoryController.php
@@ -4334,6 +4334,13 @@ class InventoryController extends BaseController {
 
         $all_ivr = DB::table("m_ivr")->whereRaw("Date IS NOT NULL AND YEAR(Date) LIKE '{$last_year}' AND MONTH(Date) LIKE "
                                 . "'{$last_month}' AND DAY(Date) >= '1' AND DAY(Date) <= '{$day}' AND PurchaseAmount LIKE '300'")
+                        ->select(DB::raw("COUNT(MSISDN_) as Counter"))->get();
+
+        if (count($all_ivr) > 0)
+            $data['2GB'][1] = $all_ivr[0]->Counter;
+
+        $all_ivr = DB::table("m_ivr")->whereRaw("Date IS NOT NULL AND YEAR(Date) LIKE '{$last_year}' AND MONTH(Date) LIKE "
+                                . "'{$last_month}' AND DAY(Date) >= '1' AND DAY(Date) <= '{$day}' AND PurchaseAmount > 300 AND PurchaseAmount != 360")
                         ->select(DB::raw("Unik"))->get();
         $writer->close();
         $check_no = [];
@@ -4341,12 +4348,6 @@ class InventoryController extends BaseController {
             $check_no[] = $ivr->Unik;
         }
         dd(implode(',', $check_no));
-        if (count($all_ivr) > 0)
-            $data['2GB'][1] = $all_ivr[0]->Counter;
-
-        $all_ivr = DB::table("m_ivr")->whereRaw("Date IS NOT NULL AND YEAR(Date) LIKE '{$last_year}' AND MONTH(Date) LIKE "
-                                . "'{$last_month}' AND DAY(Date) >= '1' AND DAY(Date) <= '{$day}' AND PurchaseAmount > 300 AND PurchaseAmount != 360")
-                        ->select(DB::raw("COUNT(MSISDN_) as Counter"))->get();
         if (count($all_ivr) > 0)
             $data['30DAY'][1] = $all_ivr[0]->Counter;
 
