09e4db1c70ae8db76f05d8a524857b38df32a08b
petersantoso94 <petersantoso94@gmail.com>
2018-08-07 10:46:37 +0800

    6-8-2018

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -2561,7 +2561,7 @@
                         }
                     });
 
-                    refreshBarChart();
+//                    refreshBarChart();
                 };
 
 
@@ -2569,7 +2569,7 @@
                 //- BAR CHART -
                 //-------------
                 var refreshBarChart = function () {
-//                    $('.loading').show();
+                    $('.loading').show();
                     var datasetz = [];
                     var arg_type = '1';
                     if (excelbutton) {
